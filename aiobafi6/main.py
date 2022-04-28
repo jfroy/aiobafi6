87d68848c167c30e4aed55ed6bbad4a31c6bec89
Jean-Francois Roy <jf@devklog.net>
2022-04-28 11:49:30 -0700

    Implement generic property commit
    This patch reworks the prototype command to be able to set any property
    using proto reflection.
    
    Conveniently, the message to set a property uses the same proto as the
    query. Good job BAF.
    
    This patch also adds a flag to control proto dumping.

--- aiobafi6/main.py
+++ aiobafi6/main.py
@@ -17,41 +17,44 @@ ARGS.add_argument(
     help="device address",
 )
 ARGS.add_argument(
-    "-s",
-    "--speed",
-    action="store",
-    dest="speed",
-    help="set fan speed",
+    "-d",
+    "--dump",
+    action="store_true",
+    dest="dump",
+    help="enable proto dumping",
 )
 ARGS.add_argument(
-    "-m",
-    "--mode",
-    action="store",
-    dest="fan_mode",
-    help="set fan mode",
+    "property",
+    nargs="?",
+    help="property name",
+)
+ARGS.add_argument(
+    "value",
+    nargs="?",
+    help="property value",
 )
 
 PORT = 31415
 
 
 async def keep_alive(writer: asyncio.StreamWriter):
-    print("sending initial keep-alive command")
+    print("sending initial keep-alive")
     root = aiobafi6_pb2.Root()
     root.root2.keep_alive.SetInParent()
     writer.write(wireutils.serialize(root))
     while True:
         await asyncio.sleep(15)
-        print("sending refresh keep-alive command")
+        print("sending refresh keep-alive")
         root = aiobafi6_pb2.Root()
         root.root2.keep_alive.unknown1 = 3
         writer.write(wireutils.serialize(root))
 
 
-async def query_state(ip_addr: str):
+async def query_state(ip_addr: str, dump: bool):
     print(f"Querying all state from {ip_addr}")
     reader, writer = await asyncio.open_connection(ip_addr, PORT)
     _ = asyncio.create_task(keep_alive(writer))
-    # i = 0
+    i = 0
     while True:
         # The wire format frames protobuf messages with 0xc0, so the first `readuntil`
         # will return just that byte and the next will return the message with the
@@ -60,35 +63,28 @@ async def query_state(ip_addr: str):
         if len(raw_buf) == 1:
             continue
         buf = wireutils.remove_emulation_prevention(raw_buf[:-1])
-        # with open(f"baf_dump-{i}.bin", "wb") as f:
-        #     f.write(buf)
-        # i += 1
+        if dump:
+            with open(f"dump-query-{i}.bin", "wb") as f:
+                f.write(buf)
+            i += 1
         root = aiobafi6_pb2.Root()
         root.ParseFromString(buf)
         print(root)
 
 
-async def set_speed(ip_addr: str, speed: int):
-    if speed < 0 or speed > 7:
-        raise ValueError(f"invalid speed value: {speed}")
-    print(f"Setting speed of fan at {ip_addr} to {speed}")
+async def set_property(ip_addr: str, property: str, value: int, dump: bool):
+    print(f"Setting {property} of fan at {ip_addr} to {value}")
     root = aiobafi6_pb2.Root()
-    root.root2.command.command3.set_speed = speed
+    try:
+        setattr(root.root2.commit.property, property, value)
+    except TypeError:
+        setattr(root.root2.commit.property, property, int(value))
+    buf = wireutils.serialize(root)
+    if dump:
+        with open(f"dump-set-{property}-{value}.bin", "wb") as f:
+            f.write(buf)
     reader, writer = await asyncio.open_connection(ip_addr, PORT)
-    writer.write(wireutils.serialize(root))
-    await writer.drain()
-    writer.close()
-    await writer.wait_closed()
-
-
-async def set_fan_mode(ip_addr: str, mode: int):
-    if mode < 0 or mode > 2:
-        raise ValueError(f"invalid mode value: {mode}")
-    print(f"Setting mode of fan at {ip_addr} to {mode}")
-    root = aiobafi6_pb2.Root()
-    root.root2.command.command3.set_fan_mode = mode
-    reader, writer = await asyncio.open_connection(ip_addr, PORT)
-    writer.write(wireutils.serialize(root))
+    writer.write(buf)
     await writer.drain()
     writer.close()
     await writer.wait_closed()
@@ -97,24 +93,21 @@ async def set_fan_mode(ip_addr: str, mode: int):
 async def async_main():
     args = ARGS.parse_args()
     try:
-        # do some checking on IP address
         ip_addr = ipaddress.ip_address(args.ip_addr)
     except ValueError:
         print("invalid address ", args.ip_addr)
         return
-    if args.speed is not None:
-        await set_speed(str(ip_addr), int(args.speed))
-    elif args.fan_mode is not None:
-        await set_fan_mode(str(ip_addr), int(args.fan_mode))
+    if args.property is not None:
+        if args.value is None:
+            raise RuntimeError("must specify property value")
+        await set_property(str(ip_addr), args.property, args.value, dump=args.dump)
     else:
-        await query_state(str(ip_addr))
+        await query_state(str(ip_addr), dump=args.dump)
 
 
 def main():
-    task = asyncio.Task(async_main())
-    loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(task)
+        asyncio.run(async_main())
     except KeyboardInterrupt:
         pass
 
