2b6b4081f552a78ea92854f0dec498d4761db834
Jean-Francois Roy <jf@devklog.net>
2022-04-27 17:04:51 -0700

    Implement add_emulation_prevention

--- aiobafi6/wireutils.py
+++ aiobafi6/wireutils.py
@@ -2,6 +2,28 @@
 from __future__ import annotations
 
 
+def add_emulation_prevention(buf: bytes) -> bytes:
+    """Adds emulation prevention sequences.
+
+    The BAF i6 protocol frames its messages on a stream connection using a pair of 0xc0
+    bytes. In case a message payload contains 0xc0 bytes, all such bytes are replaced
+    with a so-called emulation prevention sequence (`0xdb 0xdc`). In case a message
+    payload contains this emulation prevention sequence itself, all `0xdb` bytes are
+    replaced with a separate emulation prevention sequence (`0xdb 0xdd`).
+
+    This function adds all such emulation prevention sequences.
+    """
+    o = bytearray()
+    for b in buf:
+        if b == 0xC0:
+            o.extend((0xDB, 0xDC))
+        elif b == 0xDB:
+            o.extend((0xDB, 0xDD))
+        else:
+            o.append(b)
+    return bytes(o)
+
+
 def remove_emulation_prevention(buf: bytes) -> bytes:
     """Removes emulation prevention sequences.
 
--- aiobafi6/wireutils_test.py
+++ aiobafi6/wireutils_test.py
@@ -3,16 +3,31 @@ from aiobafi6 import wireutils
 
 
 class TestWireUtils(unittest.TestCase):
+    def test_add_emulation_prevention(self):
+        got = wireutils.add_emulation_prevention(b"\xc0")
+        self.assertEqual(got, b"\xdb\xdc")
+        got = wireutils.add_emulation_prevention(b"\xdb")
+        self.assertEqual(got, b"\xdb\xdd")
+        got = wireutils.add_emulation_prevention(
+            b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xc0\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xc0\x08\x01\x12\x03\xb0\t\x00'
+        )
+        self.assertEqual(
+            got,
+            b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xdb\xdc\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xdb\xdc\x08\x01\x12\x03\xb0\t\x00',
+        )
+
     def test_remove_emulation_prevention(self):
-        buf = b"\xdb\xdc"
-        got = wireutils.remove_emulation_prevention(buf)
+        got = wireutils.remove_emulation_prevention(b"\xdb\xdc")
         self.assertEqual(got, b"\xc0")
-        buf = b"\xdb\xdd"
-        got = wireutils.remove_emulation_prevention(buf)
+        got = wireutils.remove_emulation_prevention(b"\xdb\xdd")
         self.assertEqual(got, b"\xdb")
-        buf = b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xdb\xdc\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xdb\xdc\x08\x01\x12\x03\xb0\t\x00'
-        got = wireutils.remove_emulation_prevention(buf)
-        self.assertEqual(got, b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xc0\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xc0\x08\x01\x12\x03\xb0\t\x00')
+        got = wireutils.remove_emulation_prevention(
+            b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xdb\xdc\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xdb\xdc\x08\x01\x12\x03\xb0\t\x00'
+        )
+        self.assertEqual(
+            got,
+            b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xc0\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xc0\x08\x01\x12\x03\xb0\t\x00',
+        )
 
 
 if __name__ == "__main__":
