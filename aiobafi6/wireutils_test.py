b16486307204382c2df6c91c6863e22366ae03db
Jean-Francois Roy <jf@devklog.net>
2022-05-07 11:02:59 -0700

    Cleanup wireutils_test given pytest is used

--- aiobafi6/wireutils_test.py
+++ aiobafi6/wireutils_test.py
@@ -1,35 +1,30 @@
-import unittest
-
-from aiobafi6 import wireutils
-
-
-class TestWireUtils(unittest.TestCase):
-    def test_add_emulation_prevention(self):
-        got = wireutils.add_emulation_prevention(b"\xc0")
-        self.assertEqual(got, b"\xdb\xdc")
-        got = wireutils.add_emulation_prevention(b"\xdb")
-        self.assertEqual(got, b"\xdb\xdd")
-        got = wireutils.add_emulation_prevention(
-            b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xc0\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xc0\x08\x01\x12\x03\xb0\t\x00'
-        )
-        self.assertEqual(
-            got,
-            b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xdb\xdc\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xdb\xdc\x08\x01\x12\x03\xb0\t\x00',
-        )
-
-    def test_remove_emulation_prevention(self):
-        got = wireutils.remove_emulation_prevention(b"\xdb\xdc")
-        self.assertEqual(got, b"\xc0")
-        got = wireutils.remove_emulation_prevention(b"\xdb\xdd")
-        self.assertEqual(got, b"\xdb")
-        got = wireutils.remove_emulation_prevention(
-            b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xdb\xdc\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xdb\xdc\x08\x01\x12\x03\xb0\t\x00'
-        )
-        self.assertEqual(
-            got,
-            b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xc0\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xc0\x08\x01\x12\x03\xb0\t\x00',
-        )
+"""Tests for wireutils."""
+from . import wireutils
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_emulation_prevention():
+    got = wireutils.add_emulation_prevention(b"\xc0")
+    assert got == b"\xdb\xdc"
+    got = wireutils.add_emulation_prevention(b"\xdb")
+    assert got == b"\xdb\xdd"
+    got = wireutils.add_emulation_prevention(
+        b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xc0\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xc0\x08\x01\x12\x03\xb0\t\x00'
+    )
+    assert (
+        got
+        == b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xdb\xdc\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xdb\xdc\x08\x01\x12\x03\xb0\t\x00'
+    )
+
+
+def test_remove_emulation_prevention():
+    got = wireutils.remove_emulation_prevention(b"\xdb\xdc")
+    assert got == b"\xc0"
+    got = wireutils.remove_emulation_prevention(b"\xdb\xdd")
+    assert got == b"\xdb"
+    got = wireutils.remove_emulation_prevention(
+        b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xdb\xdc\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xdb\xdc\x08\x01\x12\x03\xb0\t\x00'
+    )
+    assert (
+        got
+        == b'\x12+")\x12\r\n\x0bLiving Room\x12\x04\x18\xc0\xbe\x01\x12\x03\xb0\x08\x00\x12\x03\xb8\x08\x00\x12\x03\xc0\x08\x01\x12\x03\xb0\t\x00'
+    )
